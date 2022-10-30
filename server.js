ad1a83dbd50fa8726ac6ea4c433a67d835919892
Amandisen <manda.lindqvist@gmail.com>
2022-10-30 22:13:02 +0100

    Navigation, added button from about to partypopper

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const { Server } = require("ws");
 
 const PORT = process.env.PORT || 8080;
 const INDEX = "/public/index.html";
+const HOMEPAGE = "/public/homepage.html";
 
 const server = express()
 /*   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
  */  
   .use(express.static("public"))
   .get("/", (req, res) => res.sendFile(__dirname + INDEX))
+  .get("/about", (req, res) => res.sendFile(__dirname + HOMEPAGE))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
